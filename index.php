bf99e822d9aa5ab2b9f3ea51e2376a5acef78943
Michael Fleming <flemin100@gmail.com>
2017-03-28 19:36:44 +0100

    Student metric number finds name

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
-print "<h1>Module Feedback";
+print "<h1>Module Feedback</h1>";
+if (!array_key_exists('u',$_REQUEST)) {
+	print "Who are you <form><input name=u value='50200036'><submit><form>";
+	exit();
+}
 
-if () {
-	print 
-
-print "Welcome student: ".$_REQUEST['u'];
-
-
-
-
+$con = new mysqli('localhost','scott','tiger','gisq');
+if ($con->connect_error){
+	die('Connection failure');
+}
+$sql = "SELECT SPR_FNM1, SPR_SURN from INS_SPR WHERE SPR_CODE=?";
+$stmt = $con->prepare($sql)
+	or die ($con->error);
+$stmt->bind_param('s',$_REQUEST['u'])
+	or die('Bind error');
+$stmt->execute()
+	or die('execute error');
+$cur = $stmt->get_result();
+if (!($row = $cur->fetch_array())){
+	echo "Matriculation number not found";
+	exit();
+}
+print "Welcome student: ".$row[0].''.$row[1];
 ?>
