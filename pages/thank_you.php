4c1511b58a98975d09f19cf76513d41d9ca2e1fb
Michael Fleming <flemin100@gmail.com>
2017-04-07 20:21:45 +0100

    Confrimation page for user after completing questions

---
+++ pages/thank_you.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Module Feedback</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+		<link href="../css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+	<nav class="navbar navbar-default">
+					 <div class="container">
+							 <!-- Brand and toggle get grouped for better mobile display -->
+							 <div class="navbar-header">
+									 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+											 <span class="sr-only">Toggle navigation</span>
+											 <span class="icon-bar"></span>
+											 <span class="icon-bar"></span>
+											 <span class="icon-bar"></span>
+									 </button>
+									 <a class="navbar-brand" href="index.php">Moodle</a>
+							 </div>
+
+							 <!-- Collect the nav links, forms, and other content for toggling -->
+							 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									 <ul class="nav navbar-nav navbar-right">
+											 <li><a href="index.php">Home</a></li>
+											 <li><a href="#">Student Links</a></li>
+											 <li><a href="#">Staff Links</a></li>
+											 <li><a href="#">Libary</a></li>
+											 <li><a href="#">Turnitin</a></li>
+									 </ul>
+							 </div>
+							 <!-- /.navbar-collapse -->
+					 </div>
+					 <!-- /.container-fluid -->
+</nav>
+
+		<div class="container">
+
+
+
+        <h1 class="projTitle">success</h1>
+
+        <div class="jumbotron">
+           <h3 class="text-center">Thank you</h3>
+           <p class="text-center">Thanks entering your feedback on your chosen modules</p>
+</div>
+
+  			</div>
+
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="../js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
